6a62210a581895b712f6e8f8e0e50f54488d5672
DaniellaDiwa <kembodaniella@gmail.com>
2025-10-30 16:34:57 -0400

    Question4

--- exercice7.dart
+++ exercice7.dart
@@ -2,7 +2,20 @@ class Tache {
   String description;
 
   static int nombreTotal = 0;
+
   Tache(this.description) {
-    Tache.nombreTotal++;
+    nombreTotal++; //
   }
+}
+
+void main() {
+  var a = Tache("mange_tes_légumes");
+  var b = Tache("fais tes devoirs");
+  var c = Tache("va te coucher");
+
+  print(a.description);
+  print(b.description);
+  print(c.description);
+
+  print("Nombre de tâches créées : ${Tache.nombreTotal}");
 }
---
+++ exercice8.dart
@@ -0,0 +1,10 @@
+class Livre {
+  String titre;
+  String auteur;
+  int _pages = 200;
+
+  static int totalLivres = 0;
+  Livre(this.titre, this.auteur) {
+    totalLivres++;
+  }
+}
