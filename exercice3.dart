a850b29a34ff7ee5375d1d2158442f7f0e6715db
DaniellaDiwa <kembodaniella@gmail.com>
2025-10-30 16:01:37 -0400

    Question7

--- exercice3.dart
+++ exercice3.dart
@@ -8,4 +8,28 @@ class Compte {
   String get solde {
     return "${_solde.toStringAsFixed(2)} \$";
   }
+
+  void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      print("Dépôt de ${montant.toStringAsFixed(2)} \$ effectué.");
+    } else {
+      print("Erreur: Le montant du dépôt doit être positif.");
+    }
+  }
+
+  double get soldeBrut => _solde;
+}
+
+void main() {
+  var monCompte = Compte("123456");
+
+  print("Numéro de compte: ${monCompte.numeroCompte}");
+  print("Solde initial : ${monCompte.solde}");
+
+  double montantDuDepot = 150.75;
+  print(" dépôt de ${montantDuDepot.toStringAsFixed(2)} \$:");
+  monCompte.depot(montantDuDepot);
+
+  print("Solde actuel: ${monCompte.solde}");
 }
